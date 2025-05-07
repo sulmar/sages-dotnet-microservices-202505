45769bb7e4ef185e86d0c2a14f6b61570b50bcb7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-07 14:46:31 +0200

    Dodanie gateway api z użyciem biblioteki Yarp

--- src/ApiGateway/YarpApiGateway/Program.cs
+++ src/ApiGateway/YarpApiGateway/Program.cs
@@ -1,32 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 
-// (Warstwa poœrednia) Middleware
-app.Use((context, next) =>
-{
-    if (context.Request.Path == "/api/products")
-    {
-        // TODO: forward request to the http://address1 endpoint of the Product API
+// dotnet add package Yarp.ReverseProxy
 
-        context.Response.Redirect("https://localhost:7291/api/products");
-
-        return Task.CompletedTask;
-
-    }
-    else if (context.Request.Path == "/api/cart/items")
-    {
-        // TODO: forward request to the http://address2 endpoint of the Cart Items API
-        context.Response.Redirect("https://localhost:7285/api/cart/items");
-
-        return Task.CompletedTask;
-    }
-
-
-
-    return next();
-});
-
-app.MapGet("/", () => "Hello ApiGateway!");
+app.MapReverseProxy();
 
 app.MapGet("/ping", () => "Pong!");
 
