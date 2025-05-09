d412dc23f2e8431a50569aa42e21f41b3fc99110
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-09 08:44:24 +0200

    Dodanie automatycznego odnajdywania uług w Yarp

--- src/ApiGateway/YarpApiGateway/Program.cs
+++ src/ApiGateway/YarpApiGateway/Program.cs
@@ -1,6 +1,17 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// dotnet add package Microsoft.Extensions.ServiceDiscovery
+// dotnet add package Microsoft.Extensions.ServiceDiscovery.Yarp
+
+// Dodaj odnajdywanie us³ug
+builder.Services.AddServiceDiscovery();
+
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddServiceDiscoveryDestinationResolver(); // Dodaj YARP z odnajdywaniem us³ug
+
 var app = builder.Build();
 
 // dotnet add package Yarp.ReverseProxy
